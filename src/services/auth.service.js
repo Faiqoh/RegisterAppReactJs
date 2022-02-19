2d94fe8a02ed026aa36292bc4f3fe1c49c938e5b
DVG-FAIQOH <faiqoh11.fa@gmail.com>
2022-02-19 21:43:03 +0700

    update link

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "https://service-login-app.vercel.app/api/auth/";
 class AuthService {
   login(email, password) {
     return axios
